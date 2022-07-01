9b0aa933ad768378ff2fc1f85485aa6084b12b49
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-07-01 12:55:54 +0100

    Adding icons?

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return(
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        TRVL
-                    </Link>
-                </div>
-            </nav>
-        </>
-    )
+
+  const [click, setClick] = useState(false);
+
+  return(
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRVL <i className='fab fa-typo3' />
+          </Link>
+        </div>
+        <div className="menu-icon">
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Navbar
