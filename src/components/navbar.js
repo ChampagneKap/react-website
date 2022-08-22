a6edf3f3a12a2495557a13c21ad1dbcdebbb2817
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-08-22 12:16:17 +0100

    Continuing development

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,6 +5,9 @@ function Navbar() {
 
   const [click, setClick] = useState(false);
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return(
     <>
       <nav className="navbar">
@@ -13,9 +16,31 @@ function Navbar() {
             TRVL <i className='fab fa-typo3' />
           </Link>
         </div>
-        <div className="menu-icon">
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+              Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
   )
