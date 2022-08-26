e74491a91700f3f24367a7d804ac3560dcb6935a
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-08-26 12:57:31 +0100

    Adding a Home page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button } from './Button.js';
+import '../App.css';
+import './HeroSection.css';
 
 function HeroSection() {
     return (
@@ -9,9 +11,10 @@ function HeroSection() {
             <p>What are you waiting for?</p>
             <div className='hero-btns'>
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>WATCH TRAILER <i className='far fa-play-circle' /></Button>
             </div>
         </div>
-    )
+    );
 }
 
-export default HeroSection
+export default HeroSection;
